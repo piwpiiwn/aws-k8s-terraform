7252ce63639c96b374d5f618122e930f89d8c95e
j0rzsh <jorge2006nieva@gmail.com>
2020-06-27 13:01:17 +0200

    Worker 2 and its outputs

--- 03_worker_nodes.tf
+++ 03_worker_nodes.tf
@@ -71,73 +71,73 @@ resource "aws_spot_instance_request" "worker_1" {
 }
 
 ### Worker 2 ###
-# resource "aws_security_group" "worker_2" {
-#   name        = "k8s worker 2 SG"
-#   description = "Allow cluster comunication"
-#   vpc_id      = aws_vpc.k8s.id
+resource "aws_security_group" "worker_2" {
+  name        = "k8s worker 2 SG"
+  description = "Allow cluster comunication"
+  vpc_id      = aws_vpc.k8s.id
 
-#   tags = merge(
-#     var.tags,
-#     map(
-#       "Name", "k8s_worker_2"
-#     )
-#   )
-# }
+  tags = merge(
+    var.tags,
+    map(
+      "Name", "k8s_worker_2"
+    )
+  )
+}
 
-# resource "aws_security_group_rule" "worker_2_allow_ssh" {
-#   security_group_id = aws_security_group.worker_2.id
+resource "aws_security_group_rule" "worker_2_allow_ssh" {
+  security_group_id = aws_security_group.worker_2.id
 
-#   type        = "ingress"
-#   description = "Allow SSH from everywhere"
-#   from_port   = 22
-#   to_port     = 22
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-# }
+  type        = "ingress"
+  description = "Allow SSH from everywhere"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "worker_2_allow_egress" {
-#   security_group_id = aws_security_group.worker_2.id
+resource "aws_security_group_rule" "worker_2_allow_egress" {
+  security_group_id = aws_security_group.worker_2.id
 
-#   type        = "egress"
-#   description = "Allow all egress"
-#   from_port   = 0
-#   to_port     = 0
-#   protocol    = -1
-#   cidr_blocks = ["0.0.0.0/0"]
-# }
+  type        = "egress"
+  description = "Allow all egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = -1
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "worker_2_allow_master" {
-#   security_group_id = aws_security_group.worker_2.id
+resource "aws_security_group_rule" "worker_2_allow_master" {
+  security_group_id = aws_security_group.worker_2.id
 
-#   type                     = "ingress"
-#   description              = "Allow traffic from master"
-#   from_port                = 0
-#   to_port                  = 0
-#   protocol                 = -1
-#   source_security_group_id = aws_security_group.master.id
-# }
+  type                     = "ingress"
+  description              = "Allow traffic from master"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = -1
+  source_security_group_id = aws_security_group.master.id
+}
 
-# resource "aws_spot_instance_request" "worker_2" {
-#   ami           = local.master_ami
-#   spot_price    = var.master_spot_price
-#   instance_type = var.master_instance_type
-#   key_name      = var.key_name
+resource "aws_spot_instance_request" "worker_2" {
+  ami           = local.master_ami
+  spot_price    = var.master_spot_price
+  instance_type = var.master_instance_type
+  key_name      = var.key_name
 
-#   subnet_id                   = module.k8s_worker_2.subnet_id
-#   vpc_security_group_ids      = [aws_security_group.worker_2.id]
-#   associate_public_ip_address = true
+  subnet_id                   = module.k8s_worker_2.subnet_id
+  vpc_security_group_ids      = [aws_security_group.worker_2.id]
+  associate_public_ip_address = true
 
-#   root_block_device {
-#     volume_size = "20"
-#   }
+  root_block_device {
+    volume_size = "20"
+  }
 
-# wait_for_fulfillment = true
-# spot_type            = "one-time"
+  wait_for_fulfillment = true
+  spot_type            = "one-time"
 
-#   tags = merge(
-#     var.tags,
-#     map(
-#       "Name", "k8s_worker_2"
-#     )
-#   )
-# }
+  tags = merge(
+    var.tags,
+    map(
+      "Name", "k8s_worker_2"
+    )
+  )
+}
